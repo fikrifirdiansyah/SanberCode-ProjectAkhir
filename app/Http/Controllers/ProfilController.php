ab13ec853e8bc6280628ec378f8b85ce3e98b10e
fikrifirdiansyah <fikrifirdiansyah00@gmail.com>
2020-08-15 14:40:35 +0700

    auth nya

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\profil;
 use Illuminate\Http\Request;
+use Auth;
 
 class ProfilController extends Controller
 {
@@ -12,6 +13,10 @@ class ProfilController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         //
